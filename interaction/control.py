b38d50064beecf616a645ca00bd6ba2f7fe2420a
RalXYZ <19510622+RalXYZ@users.noreply.github.com>
2020-03-16 09:48:53 +0800

    Add basic interaction method Add command "quit"

--- interaction/control.py
+++ interaction/control.py
@@ -3,5 +3,13 @@ from calculation.matrix import Matrix
 
 class Control:
     @classmethod
+    def exit(cls, num):
+        print('Are you sure you want to quit? (y/n) ', end='')
+        response = input()
+        if response in ['y', 'Y']:
+            exit(num)
+        return None
+
+    @classmethod
     def add(cls):
         pass  # TODO: Define this function, Σ.
--- landing.py
+++ landing.py
@@ -1,6 +1,18 @@
-from calculation.matrix import Matrix
-from calculation.error import *
+from interaction.control import Control
 
+while True:
+    print('>>> ', end='')
+    line = input().split()
+    if len(line) != 0:
+        if line[0] in ['end', 'quit', 'exit', 'esc', 'escape']:
+            Control.exit(0)
+        # TODO elif line[0] in ['dec', 'declare']:
+        # TODO elif line[0] in ['def', 'define']:
+        # TODO elif line[0] in ['var', 'variable']:
+        # TODO elif line[0] in ['del', 'delete', 'rm', 'remove']:
+        # TODO elif line[0] in ['lis', 'list', 'ls']:
+
+'''
 A = Matrix()
 try:
     A.init()
@@ -23,3 +35,4 @@ except AttributeError as error:
     print(error)
 except MatrixAdditionError as error:
     print(error)
+    '''
